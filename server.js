eeed84cffb3c14a7b94438ec9fde3925c7a03c5a
lixy09 <li0047@hz.nl>
2024-05-30 16:37:32 +0200

    update server.js

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 // server.js
-const express = require("express");
-const path = require("path");
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 const app = express();
 const PORT = process.env.PORT || 3030;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // Serve static files from the 'dist' directory
 app.use(express.static(path.join(__dirname, 'dist')));
